fe9b06f43e87aca4fff5f9d22cb93b07f09a998f
Marck <myohalmoam@gmail.com>
2026-09-17 16:20:32 -0600

    implement basic movement logic

--- src/main/java/snake/Snake.java
+++ src/main/java/snake/Snake.java
@@ -1,7 +1,7 @@
 package snake;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 public class Snake {
 
@@ -24,8 +24,19 @@ public class Snake {
         return snakeCoordinates.getFirst();
     }
 
+    public boolean isValidMove(int[] newHead){
+        //Arrays.equals checks equality between 1D arrays
+        if (getSnakeCoordinates().size() == 1){
+            return true;
+        }
+        else if (Arrays.equals(newHead, getSnakeCoordinates().get(1))){
+            return false;
+        }
+        return true;
+    }
+
     //Snake movements
-    public void moveRight(){
+    public void changeDirection(String direction){
         //If moving right, head will change column + 1
         //Clonning every arrray inside of original coordinates and adding to copy array
         ArrayList<int[]> currentCoordinates = new ArrayList<>(getSnakeCoordinates());
@@ -33,9 +44,31 @@ public class Snake {
         for(int[] coordinate : currentCoordinates){
             copyCoordinates.add(coordinate.clone());
         }
-
-        int [] newHead = getSnakeHead().clone();
-        newHead[1]++;
+        int[] newHead = getSnakeHead().clone();
+        if ("right".equals(direction)) {
+            newHead[1]++;
+            if (!isValidMove(newHead)){
+                newHead[1]--;
+            }
+        }
+        else if("left".equals(direction)){
+            newHead[1]--;
+            if (!isValidMove(newHead)){
+                newHead[1]++;
+            }
+        }
+        else if("up".equals(direction)){
+            newHead[0]++;
+            if (!isValidMove(newHead)){
+                newHead[0]--;
+            }
+        }
+        else {
+            newHead[0]--;
+            if (!isValidMove(newHead)){
+                newHead[0]++;
+            }
+        }
 
         //Removing last element of old body
         int lastElement = copyCoordinates.size() - 1;
@@ -47,5 +80,10 @@ public class Snake {
         //Setting snake coordinates as new modified coordinates
         setSnakeCoordinates(copyCoordinates);
     }
+
+    public void move(){
+
+    }
+
 
 }

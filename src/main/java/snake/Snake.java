4edb964cc75c0a7fea156ce4e7a2a3c397304934
Marck <myohalmoam@gmail.com>
2026-09-19 14:00:33 -0600

    added basic snake functionality for increasing size when eating food

--- src/main/java/snake/Snake.java
+++ src/main/java/snake/Snake.java
@@ -8,62 +8,58 @@ public class Snake {
     private ArrayList<int[]> snakeCoordinates = new ArrayList<>();
     private int score;
 
-    public void snakeStart(){
-        snakeCoordinates.add(new int[]{5,24});
+    public void snakeStart() {
+        snakeCoordinates.add(new int[]{5, 24});
     }
 
-    public ArrayList<int[]> getSnakeCoordinates(){
+    public ArrayList<int[]> getSnakeCoordinates() {
         return snakeCoordinates;
     }
 
-    public void setSnakeCoordinates(ArrayList<int[]> newSnakeCoordinates){
+    public void setSnakeCoordinates(ArrayList<int[]> newSnakeCoordinates) {
         this.snakeCoordinates = newSnakeCoordinates;
     }
 
-    public int[] getSnakeHead(){
+    public int[] getSnakeHead() {
         return snakeCoordinates.getFirst();
     }
 
-    public boolean isValidMove(int[] newHead){
+    public boolean isValidMove(int[] newHead) {
         //Arrays.equals checks equality between 1D arrays
-        if (getSnakeCoordinates().size() == 1){
+        if (getSnakeCoordinates().size() == 1) {
             return true;
-        }
-        else if (Arrays.equals(newHead, getSnakeCoordinates().get(1))){
+        } else if (Arrays.equals(newHead, getSnakeCoordinates().get(1))) {
             return false;
         }
         return true;
     }
 
     //Snake validate legal movements
-    public int[] changeDirection(String direction){
+    public int[] changeDirection(String direction) {
         //If moving right, head will change column + 1
 
         int[] newHead = getSnakeHead().clone();
         if ("right".equals(direction)) {
             newHead[1]++;
-            if (!isValidMove(newHead)){
+            if (!isValidMove(newHead)) {
                 newHead[1]--;
             }
             return newHead;
-        }
-        else if("left".equals(direction)){
+        } else if ("left".equals(direction)) {
             newHead[1]--;
-            if (!isValidMove(newHead)){
+            if (!isValidMove(newHead)) {
                 newHead[1]++;
             }
             return newHead;
-        }
-        else if("up".equals(direction)){
+        } else if ("up".equals(direction)) {
             newHead[0]--;
-            if (!isValidMove(newHead)){
+            if (!isValidMove(newHead)) {
                 newHead[0]++;
             }
             return newHead;
-        }
-        else {
+        } else {
             newHead[0]++;
-            if (!isValidMove(newHead)){
+            if (!isValidMove(newHead)) {
                 newHead[0]--;
             }
             return newHead;
@@ -72,11 +68,11 @@ public class Snake {
     }
 
     //Move the snake in validated direction
-    public void move(int[] direction){
+    public void move(int[] direction) {
         //Clonning every arrray inside of original coordinates and adding to copy array
         ArrayList<int[]> currentCoordinates = new ArrayList<>(getSnakeCoordinates());
         ArrayList<int[]> copyCoordinates = new ArrayList<>();
-        for(int[] coordinate : currentCoordinates){
+        for (int[] coordinate : currentCoordinates) {
             copyCoordinates.add(coordinate.clone());
         }
 
@@ -91,5 +87,52 @@ public class Snake {
         setSnakeCoordinates(copyCoordinates);
 
     }
+
+    public int[] addTail(String direction) {
+
+        //Enhanced switch usage, switch can be used as an expression
+        String oppositeDirection = switch (direction) {
+            case "right" -> "left";
+            case "left" -> "right";
+            case "up" -> "down";
+            case "down" -> "up";
+            default -> "";
+        };
+        int[] newTail = getSnakeCoordinates().getLast().clone();
+
+        if ("left".equals(oppositeDirection)) {
+            newTail[1]--;
+            return newTail;
+        } else if ("right".equals(oppositeDirection)) {
+            newTail[1]++;
+            return newTail;
+        } else if ("down".equals(oppositeDirection)) {
+            newTail[0]++;
+            return newTail;
+        } else {
+            newTail[0]--;
+            return newTail;
+        }
+
+    }
+
+    public void eatFood(Food currentFood, String direction) {
+        int [] foodCords = currentFood.getCoordinates();
+
+        if (Arrays.equals(getSnakeHead(), foodCords)) {
+            int [] newTail = addTail(direction);
+
+            ArrayList<int[]> newCoordinates = new ArrayList<>(getSnakeCoordinates());
+            newCoordinates.add(newTail);
+
+            setSnakeCoordinates(newCoordinates);
+        }
+    }
+
+    
+
+
+
+
 
 }

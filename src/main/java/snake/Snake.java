9d9443042ac6de5a755562058eeedb0046ec884c
Marck <myohalmoam@gmail.com>
2026-09-18 21:43:53 -0600

    refactored basic movement functionality into a more coherent structure

--- src/main/java/snake/Snake.java
+++ src/main/java/snake/Snake.java
@@ -35,39 +35,49 @@ public class Snake {
         return true;
     }
 
-    //Snake movements
-    public void changeDirection(String direction){
+    //Snake validate legal movements
+    public int[] changeDirection(String direction){
         //If moving right, head will change column + 1
-        //Clonning every arrray inside of original coordinates and adding to copy array
-        ArrayList<int[]> currentCoordinates = new ArrayList<>(getSnakeCoordinates());
-        ArrayList<int[]> copyCoordinates = new ArrayList<>();
-        for(int[] coordinate : currentCoordinates){
-            copyCoordinates.add(coordinate.clone());
-        }
+
         int[] newHead = getSnakeHead().clone();
         if ("right".equals(direction)) {
             newHead[1]++;
             if (!isValidMove(newHead)){
                 newHead[1]--;
             }
+            return newHead;
         }
         else if("left".equals(direction)){
             newHead[1]--;
             if (!isValidMove(newHead)){
                 newHead[1]++;
             }
+            return newHead;
         }
         else if("up".equals(direction)){
             newHead[0]++;
             if (!isValidMove(newHead)){
                 newHead[0]--;
             }
+            return newHead;
         }
         else {
             newHead[0]--;
             if (!isValidMove(newHead)){
                 newHead[0]++;
             }
+            return newHead;
+        }
+
+    }
+
+    //Move the snake in validated direction
+    public void move(int[] direction){
+        //Clonning every arrray inside of original coordinates and adding to copy array
+        ArrayList<int[]> currentCoordinates = new ArrayList<>(getSnakeCoordinates());
+        ArrayList<int[]> copyCoordinates = new ArrayList<>();
+        for(int[] coordinate : currentCoordinates){
+            copyCoordinates.add(coordinate.clone());
         }
 
         //Removing last element of old body
@@ -75,15 +85,11 @@ public class Snake {
         copyCoordinates.remove(lastElement);
 
         //Setting new body head and moving body by one position in consequence
-        copyCoordinates.addFirst(newHead.clone());
+        copyCoordinates.addFirst(direction);
 
         //Setting snake coordinates as new modified coordinates
         setSnakeCoordinates(copyCoordinates);
-    }
-
-    public void move(){
 
     }
-
 
 }
